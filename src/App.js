587ee1991a2390d10ac1f768dce1540bf4b42fc8
Roibu Vadim <roibu.vadim@gmail.com>
2021-02-26 08:53:26 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Particles from 'react-particles-js'
-import Clarifai from 'clarifai'
 import Navigation from './Components/Navigation/Navigation'
 import Signin from './Components/Signin/Signin'
 import Logo from './Components/Logo/Logo'
@@ -9,10 +8,6 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition'
 import Rank from './Components/Rank/Rank'
 import './App.css';
-
-const app = new Clarifai.App({
-  apiKey: '335ed4bd05a34238a515acb0bdf49008'
-})
 
 const particlesOptions = {
   particles: {
@@ -26,6 +21,21 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super()
@@ -34,8 +44,25 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -59,20 +86,38 @@ class App extends Component {
     this.setState({input: event.target.value})
   }
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input})
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input
-      )
-      .then(response =>  this.displayFaceBox(this.calculateFaceLocation(response)))
+    fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
+      .then(response =>  {
+        if (response){
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+            .catch(console.log)
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))})
       .catch(err => console.log(err))
   }
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     }else if (route === 'home'){
       this.setState({isSignedIn: true})
     }
@@ -90,18 +135,18 @@ class App extends Component {
         { route === 'home' 
         ? <div>
         <Logo />
-        <Rank />
+        <Rank name={this.state.user.name} entries={this.state.user.entries}/>
         <ImageLinkForm
           onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit} />
+          onPictureSubmit={this.onPictureSubmit} />
         <FaceRecognition 
           box={box} 
           imageUrl={imageUrl}/>
           </div>
         : (
           route === 'register' 
-          ? <Register onRouteChange={this.onRouteChange}/>
-          : <Signin onRouteChange={this.onRouteChange}/>
+          ? <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          : <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
         )
         
       }
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ImageLinkForm.css'
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
     return (
         <div>
             <p className='f3'>
@@ -11,7 +11,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
                 <div className='pa4 br3 shadow-5 center form'>
                     <input className='f4 pa2 w-70 center' type="text" onChange={onInputChange}/>
                     <button className='w-30 grow f4 link ph3 pv3 dib white bg-light-purple'
-                    onClick={onButtonSubmit}
+                    onClick={onPictureSubmit}
                     >Detect</button>
                 </div>
             </div>
